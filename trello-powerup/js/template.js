bfc6d5e558f38d4e35445f793f22c61a66d57e9d
joshuadutton <joshuadutton@gmail.com>
2017-03-23 16:43:53 -0600

    Take out promise dependencies

--- trello-powerup/js/template.js
+++ trello-powerup/js/template.js
@@ -1,35 +1,33 @@
-var Promise = TrelloPowerUp.Promise;
-// var WHITE_ICON = './images/icon-white.svg';
-// var GRAY_ICON = './images/icon-gray.svg';
+
 var cardEndpoint = 'https://ygrstg2qvi.execute-api.us-west-2.amazonaws.com/dev/card/';
 
-function makeRequest (method, url) {
-  return new Promise(function (resolve, reject) {
-    var xhr = new XMLHttpRequest();
-    xhr.open(method, url);
-    xhr.onload = function () {
-      if (this.status >= 200 && this.status < 300) {
-        resolve(xhr.response);
-      } else {
-        reject({
-          status: this.status,
-          statusText: xhr.statusText
-        });
-      }
-    };
-    xhr.onerror = function () {
-      reject({
-        status: this.status,
-        statusText: xhr.statusText
-      });
-    };
-    xhr.send();
-  });
-}
-
-function getCard(cardId) {
-  return makeRequest('GET', cardEndpoint+cardId).then(JSON.parse);
-}
+// function makeRequest (method, url) {
+//   return new Promise(function (resolve, reject) {
+//     var xhr = new XMLHttpRequest();
+//     xhr.open(method, url);
+//     xhr.onload = function () {
+//       if (this.status >= 200 && this.status < 300) {
+//         resolve(xhr.response);
+//       } else {
+//         reject({
+//           status: this.status,
+//           statusText: xhr.statusText
+//         });
+//       }
+//     };
+//     xhr.onerror = function () {
+//       reject({
+//         status: this.status,
+//         statusText: xhr.statusText
+//       });
+//     };
+//     xhr.send();
+//   });
+// }
+//
+// function getCard(cardId) {
+//   return makeRequest('GET', cardEndpoint+cardId).then(JSON.parse);
+// }
 
 var getBadges = function(t) {
   return t.card('id').get('id').then(function(cardId) {
